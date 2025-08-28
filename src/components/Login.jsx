b533dd693d7e1e2a6e468aac110c120b09edb5cd
shivam-work74 <shivam.work7488@gmail.com>
2025-08-28 19:51:03 +0530

    changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,7 +8,7 @@ export default function Login({ onSwitch, onSuccess }) {
   async function handleLogin(e) {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://game-server-z47n.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -9,7 +9,7 @@ export default function Register({ onSwitch, onSuccess }) {
   async function handleRegister(e) {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch("https://game-server-z47n.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
